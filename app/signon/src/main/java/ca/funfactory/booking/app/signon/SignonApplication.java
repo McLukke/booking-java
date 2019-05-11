85a5a4b0d6be4b99cbdbc8e84d4f1d917f5cd17a
jimmymlin1 <jmhlin@hotmail.com>
2019-05-11 10:24:17 -0400

    refactor already :D

---
+++ app/signon/src/main/java/ca/funfactory/booking/app/signon/SignonApplication.java
@@ -0,0 +1,13 @@
+package ca.funfactory.booking.app.signon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignonApplication.class, args);
+	}
+
+}
